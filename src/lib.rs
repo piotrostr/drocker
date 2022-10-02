f31ea80e37f4ffabcb4d0be3a95dc4476e61e25d
Piotr Ostrowski <piotrostr@google.com>
2022-10-02 17:00:27 +0200

    feat: add write_dockerfile, base cli usage, add Dockerfile

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use log::info;
 use std::fs::File;
-use std::io::{BufReader, Error, Read};
+use std::io::{BufReader, BufWriter, Error, Read, Write};
+use std::path::Path;
 
 /// Assumes that is called from the root of the project
 pub fn read_in_template(fname: &str) -> Result<String, Error> {
@@ -11,4 +12,21 @@ pub fn read_in_template(fname: &str) -> Result<String, Error> {
     buf_reader.read_to_string(&mut contents)?;
     info!("Read in {} bytes", contents.len());
     return Ok(contents.to_string());
+}
+
+pub fn write_dockerfile(contents: String) -> Result<(), Error> {
+    info!("Writing Dockerfile");
+    let file = File::create("Dockerfile")?;
+    let mut buf_writer = BufWriter::new(file);
+    buf_writer.write_all(contents.as_bytes())?;
+    Ok(())
+}
+
+pub fn dockerfile_exists() -> bool {
+    info!("Checking if Dockerfile exists");
+    Path::new("Dockerfile").exists()
+}
+
+pub fn add_args() {
+    info!("Adding args");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use rocker::read_in_template;
+use rocker::{dockerfile_exists, read_in_template, write_dockerfile};
 
 fn main() {
     env_logger::init();
 
+    if dockerfile_exists() {
+        println!("Dockerfile already exists");
+        return;
+    }
+
     let template = read_in_template("rust.tpl").ok().unwrap();
-    println!("{}", template);
+
+    match write_dockerfile(template) {
+        Ok(_) => println!("Wrote Dockerfile"),
+        Err(e) => println!("Error writing Dockerfile: {}", e),
+    }
 }
